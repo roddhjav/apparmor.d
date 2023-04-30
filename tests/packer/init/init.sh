bb69746d7f142112fec18adc3b90409a6224004b
Alexandre Pujol <alexandre@pujol.io>
2023-04-30 16:26:16 +0100

    test(packer): add image for opensuse & some cleanup.

--- tests/packer/init/init.sh
+++ tests/packer/init/init.sh
@@ -5,24 +5,37 @@
 
 set -eu
 
-# shellcheck source=/dev/null
-_lsb_release() { . /etc/os-release; echo "$ID"; }
+_lsb_release() {
+	# shellcheck source=/dev/null
+	. /etc/os-release
+	echo "$ID"
+}
 DISTRIBUTION="$(_lsb_release)"
 readonly SRC=/tmp/src
 readonly DISTRIBUTION
 
 main() {
-	install -dm0750 -o "$SUDO_USER" -g "$SUDO_USER" "/home/$SUDO_USER/Projects/" "/home/$SUDO_USER/.config/" 
+	install -dm0750 -o "$SUDO_USER" -g "$SUDO_USER" "/home/$SUDO_USER/Projects/" "/home/$SUDO_USER/.config/"
 	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/.bash_aliases "/home/$SUDO_USER/.bash_aliases"
 	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/htoprc "/home/$SUDO_USER/.config/htop/htoprc"
 	install -Dm0644 $SRC/parser.conf /etc/apparmor/parser.conf
-	install -Dm0644 $SRC/site.local /etc/apparmor.d/tunables/etc.d/site.local
+	install -Dm0644 $SRC/site.local /etc/apparmor.d/tunables/multiarch.d/site.local
 	install -Dm0755 $SRC/aa-update /usr/bin/aa-update
 	chown -R "$SUDO_USER:$SUDO_USER" "/home/$SUDO_USER/.config/"
 	case "$DISTRIBUTION" in
-	debian | ubuntu)	dpkg -i $SRC/apparmor.d_*_all.deb ;;
-	opensuse*)			zypper install -y bash-completion git go htop make rsync vim ;;
-	arch)				pacman --noconfirm -U $SRC/apparmor.d-*-x86_64.pkg.tar.zst ;;
+	arch) pacman --noconfirm -U $SRC/apparmor.d-*-x86_64.pkg.tar.zst ;;
+	debian | ubuntu)
+		apt-get update -y
+		apt-get install -y apparmor-profiles build-essential config-package-dev \
+			debhelper devscripts htop qemu-guest-agent rsync vim
+		dpkg -i $SRC/apparmor.d_*_all.deb
+		;;
+
+	opensuse*)
+		zypper install -y bash-completion git go htop make rsync vim
+		sed -i -e '/cache-loc/d' /etc/apparmor/parser.conf
+		;;
+
 	esac
 }
 
