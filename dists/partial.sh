a8d8127c73bbb5039897a9f3f3e69299e2e95aea
monsieuremre <130907164+monsieuremre@users.noreply.github.com>
2023-10-28 10:24:35 +0000

    even more fix

--- dists/partial.sh
+++ dists/partial.sh
@@ -3,33 +3,39 @@ DESTDIR=/
         
 for profile in "$@"
 do
-  echo "Installing profile $profile"
-  cp $BUILD/apparmor.d/$profile $DESTDIR/etc/apparmor.d/
-  grep "rPx," "${BUILD}/apparmor.d/${profile}" | while read line
-  do
-    dep=$(echo "$l1" | awk '{print $1}')
-    dep=$(echo $dep | awk -F"/" '{print $NF}')
-    dep=$(eval "ls ${BUILD}/apparmor.d/${dep} 2>/dev/null")
-  	for i in $dep
+	echo "Installing profile $profile"
+	cp $BUILD/apparmor.d/$profile $DESTDIR/etc/apparmor.d/
+  	grep "rPx," "${BUILD}/apparmor.d/${profile}" | while read line
   	do
-  	  i=$(echo $i | awk -F"/" '{print $NF}')
-  	  if [ ! -f "$DESTDIR/etc/apparmor.d/$i" ]; then
-        bash "$0" "$i"
-      fi
-	  done
-  done
-  grep "rPx -> " "${BUILD}/apparmor.d/${profile}" | while read line
-  do
-    dep=${line%%#*}
-    dep=$(echo $dep | awk '{print $NF}')
-    dep=${dep::-1}
-    dep=$(eval "ls ${BUILD}/apparmor.d/${dep} 2>/dev/null")
-	  for i in $dep
-	  do
-	    i=$(echo $i | awk -F"/" '{print $NF}')
-	    if [ ! -f "$DESTDIR/etc/apparmor.d/$i" ]; then
-			  bash "$0" "$i"
-      fi
-    done
-  done
+  		if [[ -z "$dep" ]]; then
+ 			continue
+		fi
+	    	dep=$(echo "$l1" | awk '{print $1}')
+	    	dep=$(echo $dep | awk -F"/" '{print $NF}')
+		dep=$(eval "ls ${BUILD}/apparmor.d/${dep} 2>/dev/null")
+	  	for i in $dep
+	  	do
+	  		i=$(echo $i | awk -F"/" '{print $NF}')
+	  		if [ ! -f "$DESTDIR/etc/apparmor.d/$i" ]; then
+				bash "$0" "$i"
+			fi
+	  	done
+	done
+	grep "rPx -> " "${BUILD}/apparmor.d/${profile}" | while read line
+	do
+   		if [[ -z "$dep" ]]; then
+ 			continue
+		fi
+		dep=${line%%#*}
+		dep=$(echo $dep | awk '{print $NF}')
+		dep=${dep::-1}
+		dep=$(eval "ls ${BUILD}/apparmor.d/${dep} 2>/dev/null")
+	  		for i in $dep
+	  		do
+	    			i=$(echo $i | awk -F"/" '{print $NF}')
+				if [ ! -f "$DESTDIR/etc/apparmor.d/$i" ]; then
+			  		bash "$0" "$i"
+	      			fi
+			done
+	done
 done
