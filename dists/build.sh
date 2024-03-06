09cdf6158d48b91badbd6ceceefeb8f898a25c74
Alexandre Pujol <alexandre@pujol.io>
2024-03-06 12:51:08 +0000

    build(debian): add lintian check.

--- dists/build.sh
+++ dists/build.sh
@@ -22,6 +22,7 @@ main() {
     dpkg)
         dch --newversion="$VERSION-1" --urgency=medium --distribution=stable --controlmaint "Release $VERSION-1"
         dpkg-buildpackage -b -d --no-sign
+        lintian || true
         mv ../"${PKGNAME}_${VERSION}-1"_*.deb "$OUTPUT"
         ;;
 
