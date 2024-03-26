f8d970faf016a93f13c4b49c3a0f1423b4c3bcc7
Alexandre Pujol <alexandre@pujol.io>
2024-03-26 18:05:55 +0000

    build: new structure for internal config files.

--- pkg/prebuild/cfg/directories.go
+++ pkg/prebuild/cfg/directories.go
@@ -19,9 +19,18 @@ var (
 	// FlagDir is the directory where the flags are stored
 	FlagDir *paths.Path = DistDir.Join("flags")
 
+	// IgnoreDir is the directory where the ignore files are stored
+	IgnoreDir *paths.Path = DistDir.Join("ignore")
+
 	// SystemdDir is the directory where the systemd drop-in files are stored
 	SystemdDir *paths.Path = paths.New("systemd")
 
+	// DebianDir is the directory where the debian specific files are stored
+	DebianDir *paths.Path = DistDir.Join("debian")
+
 	// Either or not overwrite some upstreamed profile
-	Overwrite bool = false
+	Overwrite = Overwriter{Enabled: false}
+
+	Ignore = Ignorer{}
+	Flags  = Flagger{}
 )
