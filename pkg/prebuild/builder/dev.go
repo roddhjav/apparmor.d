7a1d150a7312b233f27ba7bb889000ce01e77361
Alexandre Pujol <alexandre@pujol.io>
2024-03-30 18:17:55 +0000

    fix(profile): temporary allow  environment in profile transition.
    Turn out this is the actual source of issues #80 #235:
    - Some programs do not start well with scrub the environment
    - Not related to dbus (but dbus was affected)
    - May concern a lot of profiles
    
    As a temporary solution, we convert all Px in px while we find out a proper solution.

---
+++ pkg/prebuild/builder/dev.go
@@ -0,0 +1,35 @@
+// apparmor.d - Full set of apparmor profiles
+// Copyright (C) 2021-2024 Alexandre Pujol <alexandre@pujol.io>
+// SPDX-License-Identifier: GPL-2.0-only
+
+package builder
+
+import (
+	"github.com/roddhjav/apparmor.d/pkg/prebuild/cfg"
+	"github.com/roddhjav/apparmor.d/pkg/util"
+)
+
+var (
+	regDev = util.ToRegexRepl([]string{
+		`PUx`, `pux`,
+		`Px`, `px`,
+		`Ux`, `ux`,
+	})
+)
+
+type Dev struct {
+	cfg.Base
+}
+
+func init() {
+	RegisterBuilder(&Dev{
+		Base: cfg.Base{
+			Keyword: "dev",
+			Msg:     "Apply test development changes",
+		},
+	})
+}
+
+func (b Dev) Apply(profile string) string {
+	return regDev.Replace(profile)
+}
