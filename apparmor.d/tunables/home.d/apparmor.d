2418c1e0ddcb25618f1e4c5c0f06a7f9a9a600f4
Stoppedpuma <58333920+Stoppedpuma@users.noreply.github.com>
2024-06-14 20:41:22 +0200

    Reorganise home.d tunables

--- apparmor.d/tunables/home.d/apparmor.d
+++ apparmor.d/tunables/home.d/apparmor.d
@@ -12,22 +12,22 @@
 # First part, second part in /etc/apparmor.d/tunables/xdg-user-dirs.d/apparmor.d
 
 # Extra user personal directories
-@{XDG_BOOKS_DIR}="Books"
-@{XDG_PROJECTS_DIR}="Projects"
-@{XDG_WORK_DIR}="Work"
-@{XDG_SYNC_DIR}="Sync"
-@{XDG_TORRENTS_DIR}="Torrents"
-@{XDG_GAMES_DIR}=".games"
-@{XDG_VM_DIR}=".vm"
-@{XDG_VM_SHARES_DIR}="VM_Shares"
 @{XDG_IMG_DIR}="images"
-@{XDG_MAIL_DIR}="Mail" ".{m,M}ail"
 @{XDG_SCREENSHOTS_DIR}="Pictures/Screenshots"
 @{XDG_WALLPAPERS_DIR}="Pictures/Wallpapers"
+@{XDG_BOOKS_DIR}="Books"
+@{XDG_GAMES_DIR}=".games"
+@{XDG_PROJECTS_DIR}="Projects"
+@{XDG_WORK_DIR}="Work"
+@{XDG_MAIL_DIR}="Mail" ".{m,M}ail"
+@{XDG_SYNC_DIR}="Sync"
+@{XDG_TORRENTS_DIR}="Torrents"
+@{XDG_VM_DIR}=".vm"
+@{XDG_VM_SHARES_DIR}="VM_Shares"
 
 # User personal keyrings
-@{XDG_SSH_DIR}=".ssh"
 @{XDG_GPG_DIR}=".gnupg"
+@{XDG_SSH_DIR}=".ssh"
 @{XDG_PASSWORD_STORE_DIR}=".password-store"
 
 # User personal private directories
@@ -44,9 +44,9 @@
 # Full path of the user configuration directories
 @{user_cache_dirs}=@{HOME}/@{XDG_CACHE_DIR}
 @{user_config_dirs}=@{HOME}/@{XDG_CONFIG_DIR}
-@{user_state_dirs}=@{HOME}/@{XDG_STATE_DIR}
 @{user_bin_dirs}=@{HOME}/@{XDG_BIN_DIR}
 @{user_lib_dirs}=@{HOME}/@{XDG_LIB_DIR}
+@{user_state_dirs}=@{HOME}/@{XDG_STATE_DIR}
 
 # User build directories and output
 @{user_build_dirs}="/tmp/build/"
@@ -57,11 +57,11 @@
 # Other user directories
 @{user_books_dirs}=@{HOME}/@{XDG_BOOKS_DIR} @{MOUNTS}/@{XDG_BOOKS_DIR}
 @{user_games_dirs}=@{HOME}/@{XDG_GAMES_DIR} @{MOUNTS}/@{XDG_GAMES_DIR}
+@{user_private_dirs}=@{HOME}/@{XDG_PRIVATE_DIR} @{MOUNTS}/@{XDG_PRIVATE_DIR}
+@{user_password_store_dirs}=@{HOME}/@{XDG_PASSWORD_STORE_DIR} @{MOUNTS}/@{XDG_PASSWORD_STORE_DIR}
+@{user_work_dirs}=@{HOME}/@{XDG_WORK_DIR} @{MOUNTS}/@{XDG_WORK_DIR}
 @{user_mail_dirs}=@{HOME}/@{XDG_MAIL_DIR} @{MOUNTS}/@{XDG_MAIL_DIR}
 @{user_projects_dirs}=@{HOME}/@{XDG_PROJECTS_DIR} @{MOUNTS}/@{XDG_PROJECTS_DIR}
 @{user_sync_dirs}=@{HOME}/@{XDG_SYNC_DIR} @{MOUNTS}/*/@{XDG_SYNC_DIR}
 @{user_torrents_dirs}=@{HOME}/@{XDG_TORRENTS_DIR} @{MOUNTS}/@{XDG_TORRENTS_DIR}
 @{user_vm_dirs}=@{HOME}/@{XDG_VM_DIR} @{MOUNTS}/@{XDG_VM_DIR}
-@{user_work_dirs}=@{HOME}/@{XDG_WORK_DIR} @{MOUNTS}/@{XDG_WORK_DIR}
-@{user_password_store_dirs}=@{HOME}/@{XDG_PASSWORD_STORE_DIR} @{MOUNTS}/@{XDG_PASSWORD_STORE_DIR}
-@{user_private_dirs}=@{HOME}/@{XDG_PRIVATE_DIR} @{MOUNTS}/@{XDG_PRIVATE_DIR}
