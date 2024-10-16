5547b9875a8c773764442497521c7baec9ee3ecc
Alexandre Pujol <alexandre@pujol.io>
2024-10-16 23:58:48 +0100

    tests: small fixes on builders scripts.

--- tests/packer/init/init.sh
+++ tests/packer/init/init.sh
@@ -15,6 +15,16 @@ readonly SRC=/tmp/src
 readonly DISTRIBUTION
 
 main() {
+	install -dm0750 -o "$SUDO_USER" -g "$SUDO_USER" "/home/$SUDO_USER/Projects/" "/home/$SUDO_USER/Projects/apparmor.d" "/home/$SUDO_USER/.config/"
+	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/.bash_aliases "/home/$SUDO_USER/.bash_aliases"
+	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/monitors.xml "/home/$SUDO_USER/.config/monitors.xml"
+	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/htoprc "/home/$SUDO_USER/.config/htop/htoprc"
+	install -Dm0644 $SRC/site.local /etc/apparmor.d/tunables/multiarch.d/site.local
+	install -Dm0755 $SRC/aa-update /usr/bin/aa-update
+	install -Dm0755 $SRC/aa-log-clean /usr/bin/aa-log-clean
+	cat $SRC/parser.conf >>/etc/apparmor/parser.conf
+	chown -R "$SUDO_USER:$SUDO_USER" "/home/$SUDO_USER/.config/"
+
 	case "$DISTRIBUTION" in
 	arch)
 		pacman --noconfirm -U $SRC/*.pkg.tar.zst
@@ -35,16 +45,6 @@ main() {
 		;;
 
 	esac
-
-	install -dm0750 -o "$SUDO_USER" -g "$SUDO_USER" "/home/$SUDO_USER/Projects/" "/home/$SUDO_USER/Projects/apparmor.d" "/home/$SUDO_USER/.config/"
-	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/.bash_aliases "/home/$SUDO_USER/.bash_aliases"
-	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/monitors.xml "/home/$SUDO_USER/.config/monitors.xml"
-	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/htoprc "/home/$SUDO_USER/.config/htop/htoprc"
-	install -Dm0644 $SRC/site.local /etc/apparmor.d/tunables/multiarch.d/site.local
-	install -Dm0755 $SRC/aa-update /usr/bin/aa-update
-	install -Dm0755 $SRC/aa-log-clean /usr/bin/aa-log-clean
-	cat $SRC/parser.conf >>/etc/apparmor/parser.conf
-	chown -R "$SUDO_USER:$SUDO_USER" "/home/$SUDO_USER/.config/"
 }
 
 main "$@"
