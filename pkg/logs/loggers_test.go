7e09351f8f7bc905ff5ad4f892879ab3d51fb93d
Alexandre Pujol <alexandre@pujol.io>
2024-10-21 14:12:02 +0100

    feat(aa-log): add the --since option.

--- pkg/logs/loggers_test.go
+++ pkg/logs/loggers_test.go
@@ -49,7 +49,7 @@ func TestGetJournalctlLogs(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			reader, _ := GetJournalctlLogs(tt.path, tt.useFile)
+			reader, _ := GetJournalctlLogs(tt.path, "", tt.useFile)
 			if got := New(reader, tt.name); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("New() = %v, want %v", got, tt.want)
 			}
