147059957611f7a1d825d6f1c612a909f9a53ee8
Alexandre Pujol <alexandre@pujol.io>
2025-03-01 16:07:10 +0100

    test(packer): update sources

--- tests/packer/init.sh
+++ tests/packer/init.sh
@@ -17,12 +17,11 @@ readonly DISTRIBUTION
 main() {
 	install -dm0750 -o "$SUDO_USER" -g "$SUDO_USER" "/home/$SUDO_USER/Projects/" "/home/$SUDO_USER/Projects/apparmor.d" "/home/$SUDO_USER/.config/"
 	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/.bash_aliases "/home/$SUDO_USER/.bash_aliases"
-	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/monitors.xml "/home/$SUDO_USER/.config/monitors.xml"
 	install -Dm0644 -o "$SUDO_USER" -g "$SUDO_USER" $SRC/htoprc "/home/$SUDO_USER/.config/htop/htoprc"
+	install -Dm0644 $SRC/parser.conf /etc/apparmor/parser.conf
 	install -Dm0644 $SRC/site.local /etc/apparmor.d/tunables/multiarch.d/site.local
 	install -Dm0755 $SRC/aa-update /usr/bin/aa-update
-	install -Dm0755 $SRC/aa-log-clean /usr/bin/aa-log-clean
-	cat $SRC/parser.conf >>/etc/apparmor/parser.conf
+	install -Dm0755 $SRC/aa-clean /usr/bin/aa-clean
 	chown -R "$SUDO_USER:$SUDO_USER" "/home/$SUDO_USER/.config/"
 
 	case "$DISTRIBUTION" in
