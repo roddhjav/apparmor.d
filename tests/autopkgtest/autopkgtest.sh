dee0b867910f3ab821881daddbbac74de841f849
Alexandre Pujol <alexandre@pujol.io>
2025-10-24 21:34:58 +0200

    tests: update autopkgtests script.

--- tests/autopkgtest/autopkgtest.sh
+++ tests/autopkgtest/autopkgtest.sh
@@ -23,8 +23,8 @@ readonly FLAVOR="test"
 readonly VERBOSE=${VERBOSE:-0}
 
 # The maximum the host can handle
-readonly CPUS=32
-readonly RAM=76800
+readonly CPUS=16
+readonly RAM=16384
 readonly TIMEOUT=1800
 
 # As defined in Justfile
@@ -38,13 +38,13 @@ readonly OUTPUT=".logs/autopkgtest/"
 readonly VM_PATH="$VM_DIR/${PREFIX}${OSINFO}-${FLAVOR}.qcow2"
 readonly PACKAGES_FILE="tests/autopkgtest/src-packages"
 readonly reset='\e[0m' red='\e[0;31m' magenta='\e[0;35m'
-mapfile -t PACKAGES <"$PACKAGES_FILE"
 
 _message() { printf '%b%s%b\n' "$magenta" "$*" "$reset" >&2; }
 _verbose() { printf '%b>%b %s\n' "$magenta" "$reset" "$*" >&2; }
 _log() { printf '%b%s%b\n' "$red" "$*" "$reset" >&2; }
 
 _run() {
+	local pkg=""
 	coproc C { unbuffer -p ./tests/autopkgtest/autopkgtest.sh test "$OSINFO" 2>&1; }
 	CMD_PID=$!
 	while IFS= read -r line <&"${C[0]}"; do
@@ -52,11 +52,15 @@ _run() {
 		if [[ $VERBOSE -eq 0 ]]; then
 			_verbose "$line"
 		fi
+		if [[ $line == *">>>> Testing package "* ]]; then
+			pkg="${line#*">>>> Testing package "}"
+			pkg="-${pkg%% <<<<*}"
+		fi
 		if [[ $line == "Press Enter to resume running tests." ]]; then
 			# shellcheck disable=SC2086
 			ssh -n $SSH_OPT -p 10022 "$USER@localhost" sudo aa-log --raw | while IFS= read -r log; do
 				_log "$log"
-				echo "$log" >>"$OUTPUT/aa-log-$(date +%Y%m%d-%H%M%S)"
+				echo "$log" >>"$OUTPUT/aa-log-$(date +%Y%m%d-%H%M%S)$pkg"
 			done
 			printf '\n' >&"${C[1]}" # send Enter back over the PTY
 		fi
@@ -65,13 +69,15 @@ _run() {
 }
 
 _autopkgtest() {
-	local start_from="abook"
+	local start_from="openvpn"
 	local end_at="xfsprogs"
-	for pkg in "${PACKAGES[@]}"; do
+	mapfile -t packages <"$PACKAGES_FILE"
+
+	for pkg in "${packages[@]}"; do
 		[[ "$pkg" < "$start_from" ]] && continue
 		[[ "$pkg" > "$end_at" ]] && break
 		_message ">>>> Testing package $pkg <<<<"
-		autopkgtest "$pkg" --shell --timeout=$TIMEOUT \
+		autopkgtest "$pkg" --shell --timeout=$TIMEOUT --timeout-factor=4.0 \
 			-- qemu --cpus=$CPUS --ram-size=$RAM \
 			--user="$USER" --password="$PASSWORD" \
 			"$VM_PATH" || true
