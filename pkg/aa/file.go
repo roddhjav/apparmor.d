b1cb503b1236984f8810ef13136cb925d5c583e7
Alexandre Pujol <alexandre@pujol.io>
2025-11-12 00:38:57 +0100

    tests(aa-log): improve units tests & small fixes.

--- pkg/aa/file.go
+++ pkg/aa/file.go
@@ -88,6 +88,9 @@ func newFile(q Qualifier, rule rule) (Rule, error) {
 }
 
 func newFileFromLog(log map[string]string) Rule {
+	if log["operation"] == "link" {
+		log["requested_mask"] += "l"
+	}
 	accesses, err := toAccess("file-log", log["requested_mask"])
 	if err != nil {
 		panic(fmt.Errorf("newFileFromLog(%v): %w", log, err))
