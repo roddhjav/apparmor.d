f5f7cc9793d9c908e46b1fdaacf102c5f010b6b6
Alexandre Pujol <alexandre@pujol.io>
2025-11-30 22:55:49 +0100

    tests(aa): update some tests to the last changes.

--- pkg/aa/rule_test.go
+++ pkg/aa/rule_test.go
@@ -225,7 +225,7 @@ var (
 			other:     network1,
 			wCompare:  -7,
 			wMerge:    false,
-			wString:   "network dgram ip=127.0.0.1 port=57007 peer=(ip=127.0.0.53, port=53), # failed af match",
+			wString:   "network send dgram ip=127.0.0.1 port=57007 peer=(ip=127.0.0.53, port=53), # failed af match",
 		},
 		{
 			name:     "mount",
@@ -460,7 +460,7 @@ var (
 			other:    profile2,
 			wCompare: -4,
 			wMerge:   false,
-			wString:  "profile sudo {\n}",
+			wString:  "profile sudo {\n}\n",
 		},
 		{
 			name:     "hat",
