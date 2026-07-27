c05b4a45d7dcd4f88a5b997aa8d8161b9ef8027d
Alexandre Pujol <alexandre@pujol.io>
2026-07-28 00:37:17 +0200

    build: add future option to prebuild command.

--- cmd/prebuild/main.go
+++ cmd/prebuild/main.go
@@ -71,11 +71,14 @@ func main() {
 		)).
 
 		// Ignore profiles and files from dist/ignore
-		Add(configure.NewIgnore()).
+		Add(configure.NewIgnore())
 
+	if !cli.Future() {
 		// Merge profiles (from group/, profiles-*-*/) to a unified apparmor.d directory
-		Add(configure.NewMerge()).
+		r.Configures.Add(configure.NewMerge())
+	}
 
+	r.Configures.
 		// Set distribution specificities
 		Add(configure.NewConfigure()).
 
